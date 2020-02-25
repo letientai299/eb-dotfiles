5212ff28e3defc5b64def4e1ce885f11c0991a2c
Tai <letientai299@gmail.com>
2020-02-26 05:34:58 +0800

    Fix bugs in testing directory exist

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,8 +25,8 @@ if ! command -v curl >/dev/null 2>&1; then
   exit 1
 fi
 
-if [ -d "~/.eb-dotfiles" ]; then
-  echo_error "Directory ~/.eb-dotfiles exists. Stop!"
+if [[ -d ~/.eb-dotfiles ]]; then
+  echo_error "Directory ~/.eb-dotfiles exists. Stop"
   exit 1
 fi
 
